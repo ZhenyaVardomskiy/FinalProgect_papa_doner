b7ca25f548f5f59d3f5f249adc2ea7f05d7afc62
ZhenyaVardomskiy <zhenya.vardomskiy99@gmail.com>
2023-02-28 16:29:01 +0300

    feat(FE-13): updated user interface

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -2,66 +2,101 @@ import { Box, Button } from '@mui/material'
 import { Formik } from 'formik'
 import { observer } from 'mobx-react-lite'
 import { UserModel } from 'models'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ROUTES } from 'shared/const'
 import { FormInput } from 'shared/const/ui/FormInput'
 import { emailValidation, passwordValidation } from 'shared/helpers'
 import * as Yup from 'yup'
 
+import styles from './styles.module.scss'
+
 const SignInSchema = Yup.object().shape({
-    email: emailValidation(),
-    password: passwordValidation()
+     email: emailValidation(),
+     password: passwordValidation(),
 })
 
 function User() {
-     const navigate = useNavigate()
+     const navigate = useNavigate()     
 
      return (
-          <Box
-               sx={{
-                    maxWidth: 400,
-                    borderRadius: 6,
-                    p: 4,
-               }}
-          >
-               <Formik
-                    initialValues={{
-                         email: UserModel.email,
-                         password: UserModel.password,
-                    }}
-                    validationSchema={SignInSchema}
-                    onSubmit={(values) => {
-                         UserModel.changeInfo(values)
-                    }}
-               >
-                    {({ handleSubmit }) => (
-                         <form
-                              onSubmit={handleSubmit}
-                              style={{
-                                   display: 'flex',
-                                   flexDirection: 'column',
-                                   gap: '12px',
-                              }}
-                         >
-                              <FormInput name="email" />
-                              <FormInput name="password" />
-                              <Button type="submit" variant="contained">
-                                   Submit
-                              </Button>
-                         </form>
-                    )}
-               </Formik>
-               <Button
-                    variant="outlined"
-                    color="error"
-                    onClick={() => {
-                         UserModel.logOut()
-                         navigate(ROUTES.HOME)
+          <div className={styles.wrapper}>
+               <div className={styles.titleWrapper}>
+                    <div className={styles.title}>Профиль</div>
+                    <div
+                         className={styles.logOut}
+                         onClick={() => {
+                              UserModel.logOut()
+                              navigate(ROUTES.HOME)
+                         }}
+                    >
+                         Выйти
+                    </div>
+               </div>
+               <Box
+                    sx={{
+                         maxWidth: 600,
                     }}
                >
-                    Log out
-               </Button>
-          </Box>
+                    <Formik
+                         initialValues={{
+                              name: UserModel.name,
+                              lastName: UserModel.lastName,
+                              email: UserModel.email,
+                              password: '',
+                              confirmPassword: '',
+                         }}
+                         validationSchema={SignInSchema}
+                         onSubmit={(values) => {
+                              UserModel.changeInfo(values)
+                         }}
+                    >
+                         {({ handleSubmit }) => (
+                              <form
+                                   onSubmit={handleSubmit}
+                                   style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        gap: '20px',
+                                   }}
+                              >
+                                   <div className={styles.changeInfoBlock}>
+                                        <div className={styles.subtitle}>
+                                             Личные данные
+                                        </div>
+                                        <FormInput name="name" label="Имя" />
+                                        <FormInput
+                                             name="lastName"
+                                             label="Фамилия"
+                                        />
+                                        <FormInput name="email" />
+                                   </div>
+                                   <div className={styles.changeInfoBlock}>
+                                        <div className={styles.subtitle}>
+                                             Изменение пароля
+                                        </div>
+                                        <FormInput
+                                             name="password"
+                                             label="Новый пароль"
+                                        />
+                                        <FormInput
+                                             name="confirmPassword"
+                                             label="Повторите пароль"
+                                        />
+                                        <button
+                                             className={styles.btn}
+                                             type="submit"
+                                             
+                                        >
+                                             Сохранить
+                                        </button>
+                                        
+                                   </div>
+                              </form>
+                         )}
+                    </Formik>
+               </Box>
+          </div>
      )
 }
 
